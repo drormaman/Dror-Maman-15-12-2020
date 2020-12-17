db654a2d5626a4997e8e00ce3d5f4356c50ccd4c
DrorMaman <drormaman@gmail.com>
2020-12-17 17:11:00 +0200

    changed theme colors and improved styling on mobile

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { amber, teal } from "@material-ui/core/colors";
+
+export const theme = createMuiTheme({
+	palette: {
+		primary: teal,
+		secondary: amber
+	}
+});
